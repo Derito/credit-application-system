7d62981083df37df437470ec6fa9b1f9327b1593
Hélder Costa <helderdacsta@hotmail.com>
2023-06-09 08:28:36 +0100

    Configurando o application.yml

---
+++ src/test/kotlin/com/helder/credit/aplication/system/CreditAplicationSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.helder.credit.aplication.system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditAplicationSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
